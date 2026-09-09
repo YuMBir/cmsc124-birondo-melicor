5d198d1a11f8d961bcc3b022bedced981e98c883
Yuan Birondo <ycbirondo@up.edu.ph>
2026-09-10 01:34:45 +0800

    fix: updated arg scanning with error handling

--- src/main/atelier/Main.kt
+++ src/main/atelier/Main.kt
@@ -11,16 +11,24 @@ private fun fail(message: String): Nothing {
 }
 
 fun main(args: Array<String>) {
-    val path = args.firstOrNull() ?: fail("expected one source-file path")
-
-    val source = try { //source contains the string
-        Files.readString(Path.of(path), StandardCharsets.UTF_8) //read entire contents of a text file as a single string
-    } catch (error: Exception) {
-        when (path){
-            "--tokenize" -> tokenize(scan(args[1]))
-            else -> fail("$path is an invalid flag")
+    val argsSize = args.size
+    val path: String
+    //arg count checker
+    if (argsSize == 2){
+        //run with mode
+        val mode = args[0]
+        path = args[1]
+        when (mode){
+            "--tokenize" -> tokenize(scan(path))
+            else -> fail("invalid flag")
         }
+    } else if(argsSize == 1){
+        path = args[0]
+        //placeholder for the future
+        val code = scan(path)
+        System.out.write(code.toByteArray(StandardCharsets.UTF_8))
+    } else{
+        fail("invalid no. of arguments")
     }
-
 }
 
