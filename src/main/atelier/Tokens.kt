11a49dd4b63ca8b8341062543d3c3826476108d5
Yuan Birondo <ycbirondo@up.edu.ph>
2026-09-12 18:04:27 +0800

    fix: updated Token toString to match the expected properly

--- src/main/atelier/Tokens.kt
+++ src/main/atelier/Tokens.kt
@@ -3,6 +3,6 @@ package atelier
 data class Token(val type: String, val lexeme: String, val line: Int){
     override fun toString(): String {
         //custom tokenize output format
-        return "(type=$type, lexeme=$lexeme, line=$line)\n"
+        return "Token(type=$type, lexeme=$lexeme, line=$line)\n"
     }
 }
