1f04e03275cb6e5fdf21cfcc6edc29a42fc45fb3
Yuan Birondo <ycbirondo@up.edu.ph>
2026-09-21 22:41:36 +0800

    feat: removed optimize(), will add it back later

--- src/main/atelier/Nodes.kt
+++ src/main/atelier/Nodes.kt
@@ -3,7 +3,6 @@ package atelier
 sealed interface Expr{
     override fun toString(): String
     fun eval() : Expr?
-    fun optimize()
 }
 class Group(expr: Expr) : Expr{
     override fun toString(): String {
@@ -12,7 +11,5 @@ class Group(expr: Expr) : Expr{
     override fun eval() : Expr? {
         TODO()
     }
-    override fun optimize(){
-        TODO()
-    }
 }
+
