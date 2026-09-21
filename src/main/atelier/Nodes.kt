267cdc8b2c38856464ab2edd15900e892720979d
Yuan Birondo <ycbirondo@up.edu.ph>
2026-09-21 22:56:02 +0800

    feat: string implementation for binary and literal

--- src/main/atelier/Nodes.kt
+++ src/main/atelier/Nodes.kt
@@ -2,14 +2,20 @@ package atelier
 
 sealed interface Expr{
     override fun toString(): String
-    fun eval() : Expr?
 }
 class Group(expr: Expr) : Expr{
     override fun toString(): String {
         return super.toString()
     }
-    override fun eval() : Expr? {
-        TODO()
+}
+class Literal(val literal: Any) : Expr{
+    override fun toString(): String {
+        return literal.toString()
+    }
+}
+class Binary(val left: Expr, val operator:Token, val right: Expr) : Expr{
+    override fun toString(): String {
+        return "$left ${operator.getLiteral()} $right"
     }
 }
 
