e93d7a64ce942ba0d55c14612c9903a68324c04f
Yuan Birondo <ycbirondo@up.edu.ph>
2026-09-21 23:05:32 +0800

    feat: init Parser functions

--- src/main/atelier/Parser.kt
+++ src/main/atelier/Parser.kt
@@ -1,5 +1,26 @@
 package atelier
 
 class Parser(private var tokens: List<Token>) {
+    var expressions = mutableListOf<Expr>()
+    fun printAST(){
+        for (expression in expressions){
+            println(expression)
+        }
+    }
+    fun parse(): Expr{
+        TODO()
+    }
+    fun expression(): Expr{
+        TODO()
+    }
+    fun term(): Expr{
+        TODO()
+    }
+    fun factor(): Expr{
+        TODO()
+    }
+    fun primary(): Expr{
+        TODO()
+    }
 }
 
